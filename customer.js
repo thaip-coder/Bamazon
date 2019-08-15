fcf5f97c34ffba933b6d95001ec2d47e8194913e
Thai Phan <thaiphan90@yahoo.com>
2019-08-15 17:33:26 -0500

    notes

--- customer.js
+++ customer.js
@@ -1,6 +1,8 @@
+//Node Module Variables
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
+//Variable to create connection to MySql Database
 var con = mysql.createConnection({
     host: 'localhost',
     port: '3306',
@@ -9,13 +11,14 @@ var con = mysql.createConnection({
     database: 'bamazon'
 });
 
+//Connecting to MySql Database
 con.connect(function(err) {
     if (err) throw err;
     console.log('Connected as ID: ' + con.threadId);
     con.end();
 });
 
-
+//Function to start purchase
 function buy() {
     console.table()
     inquirer.prompt({
