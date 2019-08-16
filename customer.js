73ebd1d866f28832b61bf332a5f86a5ce034b766
Thai Phan <thaiphan90@yahoo.com>
2019-08-16 16:52:49 -0500

    connected to mysql database successfully

--- customer.js
+++ customer.js
@@ -15,15 +15,24 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log('Connected as ID: ' + con.threadId);
-    con.end();
+    afterConnection();
+    purchase();
 });
 
-//Function to start purchase
-function buy() {
-    console.table()
-    inquirer.prompt({
-        name: "buy",
-        type: "list",
-        message: "What would you like to purchase?"
+//Function to start purchase after connection
+function afterConnection() {
+    con.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
     });
 };
+
+function purchase() {
+    inquirer.prompt({
+        name: "buy",
+        type: "input",
+        message: "Input ID of item you would like to purchase."
+    });
+}
+
